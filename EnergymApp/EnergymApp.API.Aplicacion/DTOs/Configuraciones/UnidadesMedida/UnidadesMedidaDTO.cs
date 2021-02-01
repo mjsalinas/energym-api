398a5df22ce12717c20c73107fe375747cb40742
Maria Salinas <mariasalinas713@gmail.com>
2021-02-01 03:03:20 -0600

    -agregado repositorio y DTO de UnidadesMedida -añadidas las referencias entre proyectos de Infraestructura-Aplicacion-Dominio

---
+++ EnergymApp/EnergymApp.API.Aplicacion/DTOs/Configuraciones/UnidadesMedida/UnidadesMedidaDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnergymApp.API.Aplicacion.DTOs.Configuraciones.UnidadesMedida
+{
+    public class UnidadesMedidaDTO
+    {
+        public int IdUnidadMedida { get; set; }
+        public string UnidadMedida { get; set; }
+        public byte? RegistroOculto { get; set; }
+        public string mensajeDeError { get; set; }
+    }
+}
---
+++ EnergymApp/EnergymApp.API.Infraestructura/Repositorios/Configuraciones/UnidadesDeMedida/UnidadesMedidaRepositorio.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EnergymApp.API.Domino.ModelosDB;
+using EnergymApp.API.Domino.Contexto;
+using System.Linq;
+using EnergymApp.API.Aplicacion.DTOs.Configuraciones.UnidadesMedida;
+
+namespace EnergymApp.API.Infraestructura.Repositorios.Configuraciones.UnidadesDeMedida
+{
+    public class UnidadesMedidaRepositorio
+    {
+        public List<UnidadesMedidaDTO> ObtenerUnidadesMedida()
+        {
+            ContextoEnergym db = new ContextoEnergym();
+            List<UnidadesMedida> unidadesMedida = db.UnidadesMedida.ToList();
+            List<UnidadesMedidaDTO> unidadesMedidaDTO = new List<UnidadesMedidaDTO>();
+
+            foreach (var unidadMedida in unidadesMedida)
+            {
+                unidadesMedidaDTO.Add(new UnidadesMedidaDTO
+                {
+                    IdUnidadMedida = unidadMedida.IdUnidadMedida,
+                    UnidadMedida = unidadMedida.UnidadMedida,
+                    RegistroOculto = unidadMedida.RegistroOculto,
+                });
+            }
+            return unidadesMedidaDTO;
+        }
+
+        public UnidadesMedidaDTO GuardarUnidadMedida(UnidadesMedidaDTO unidadMedida)
+        {
+            try
+            {
+                ContextoEnergym db = new ContextoEnergym();
+                UnidadesMedida unidadMedidaEntidad = new UnidadesMedida
+                {
+                    UnidadMedida = unidadMedida.UnidadMedida,
+                    RegistroOculto = unidadMedida.RegistroOculto
+                };
+                db.UnidadesMedida.Add(unidadMedidaEntidad);
+                db.SaveChanges();
+                unidadMedida.IdUnidadMedida = unidadMedidaEntidad.IdUnidadMedida;
+                return unidadMedida;
+            }
+            catch (Exception ex)
+            {
+                return new UnidadesMedidaDTO
+                {
+                    mensajeDeError = ex.Message
+                };
+            }
+        }
+
+        public UnidadesMedidaDTO ModificarUnidadMedida(UnidadesMedidaDTO unidadMedida)
+        {
+            try
+            {
+                ContextoEnergym db = new ContextoEnergym();
+
+                UnidadesMedida unidadMedidaEntidad = db.UnidadesMedida.FirstOrDefault(x => x.IdUnidadMedida == unidadMedida.IdUnidadMedida);
+                if(unidadMedidaEntidad == null)
+                {
+                    return new UnidadesMedidaDTO
+                    {
+                        mensajeDeError = "Unidad de Medida no existe"
+                    };
+                };
+                unidadMedidaEntidad.UnidadMedida = unidadMedida.UnidadMedida;
+                unidadMedidaEntidad.RegistroOculto = unidadMedida.RegistroOculto;
+                db.SaveChanges();
+                return unidadMedida;
+            }
+            catch (Exception ex)
+            {
+                return new UnidadesMedidaDTO
+                {
+                    mensajeDeError = ex.Message
+                };
+            }
+        }
+
+    }
+}
