59513f83afa6639a213ce625dece57a418e8fb7d
Aaron Lemus <aalberto2695@gmail.com>
2021-02-01 03:33:13 -0600

    se agrego repositorio campo seguimiento y campo seguimiento  dto.

---
+++ EnergymApp/EnergymApp.API.Aplicacion/DTOs/Configuraciones/CamposSeguimiento/CamposSeguimientoDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnergymApp.API.Aplicacion.DTOs.Configuraciones.CamposSeguimiento
+{
+    public class CamposSeguimientoDTO
+    {
+        public int IdCampoSeguimiento { get; set; }
+        public int IdUnidadMedida { get; set; }
+        public string CampoSeguimiento1 { get; set; }
+        public sbyte? RegistroOculto { get; set; }
+        public string MensajeDeError { get; set; }
+
+    }
+}
---
+++ EnergymApp/EnergymApp.API.Infraestructura/Repositorios/Configuraciones/CamposSeguimiento/CampoSeguimientoRepositorio.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EnergymApp.API.Domino.ModelosDB;
+using EnergymApp.API.Domino.Contexto;
+using System.Linq;
+using EnergymApp.API.Aplicacion.DTOs.Configuraciones.CamposSeguimiento;
+
+namespace EnergymApp.API.Infraestructura.Repositorios.Configuraciones.CamposSeguimiento
+{
+    public class CampoSeguimientoRepositorio
+    {
+        public class CamposSeguimientoRepositorio
+        {
+            private const string MensajeErrorInexistencia = "Unidad de Medida no existe";
+            public List<CamposSeguimientoDTO> ObtenerCamposSeguimiento()
+            {
+                ContextoEnergym db = new ContextoEnergym();
+                List<CampoSeguimiento> CamposSeguimiento = db.CampoSeguimiento.ToList();
+                List<CamposSeguimientoDTO> CamposSeguimientoDTO = new List<CamposSeguimientoDTO>();
+
+                foreach (var campoSeguimiento in CamposSeguimiento)
+                {
+                    CamposSeguimientoDTO.Add(new CamposSeguimientoDTO
+                    {
+                        IdCampoSeguimiento = campoSeguimiento.IdCampoSeguimiento,
+                        IdUnidadMedida = campoSeguimiento.IdUnidadMedida,
+                        CampoSeguimiento1 = campoSeguimiento.CampoSeguimiento1,
+                        RegistroOculto = campoSeguimiento.RegistroOculto,
+                    });
+                }
+                return CamposSeguimientoDTO;
+            }
+
+            public CamposSeguimientoDTO GuardarUnidadMedida(CamposSeguimientoDTO campoSeguimiento)
+            {
+                try
+                {
+                    ContextoEnergym db = new ContextoEnergym();
+                    CampoSeguimiento campoSeguimientoEntidad = new CampoSeguimiento
+                    {
+                        IdUnidadMedida = campoSeguimiento.IdUnidadMedida,
+                        CampoSeguimiento1 = campoSeguimiento.CampoSeguimiento1,
+                        RegistroOculto = campoSeguimiento.RegistroOculto,
+                    };
+                    db.CampoSeguimiento.Add(campoSeguimientoEntidad);
+                    db.SaveChanges();
+                    campoSeguimiento.IdUnidadMedida = campoSeguimientoEntidad.IdUnidadMedida;
+                    return campoSeguimiento;
+                }
+                catch (Exception ex)
+                {
+                    return new CamposSeguimientoDTO
+                    {
+                        MensajeDeError = ex.Message
+                    };
+                }
+            }
+
+            public CamposSeguimientoDTO ModificarUnidadMedida(CamposSeguimientoDTO campoSeguimiento)
+            {
+                try
+                {
+                    ContextoEnergym db = new ContextoEnergym();
+
+                    CampoSeguimiento campoSeguimientoEntidad = db.CampoSeguimiento.FirstOrDefault(camSeg => camSeg.IdCampoSeguimiento == campoSeguimiento.IdCampoSeguimiento);
+                    if (campoSeguimientoEntidad == null)
+                    {
+                        return new CamposSeguimientoDTO
+                        {
+                            MensajeDeError = MensajeErrorInexistencia
+                        };
+                    };
+                    campoSeguimientoEntidad.IdUnidadMedida = campoSeguimiento.IdUnidadMedida;
+                    campoSeguimientoEntidad.CampoSeguimiento1 = campoSeguimiento.CampoSeguimiento1;
+                    campoSeguimientoEntidad.RegistroOculto = campoSeguimiento.RegistroOculto;
+                    db.SaveChanges();
+                    return campoSeguimiento;
+                }
+                catch (Exception ex)
+                {
+                    return new CamposSeguimientoDTO
+                    {
+                        MensajeDeError = ex.Message
+                    };
+                }
+            }
+        }
+    }
+}
