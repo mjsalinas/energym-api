7326bc934d27d2b67c9e73d20c7d800599da9512
Waldo Navas <waldo9000@gmail.com>
2021-02-03 15:09:57 -0600

    Commit inicial de DatosSeguimiento.cs

---
+++ EnergymApp/EnergymApp.API.Infraestructura/Repositorios/Cliente/DatosSeguimiento/DatosSeguimiento.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnergymApp.API.Infraestructura.Repositorios.Cliente.DatosSeguimiento
+{
+    public class DatosSeguimiento
+    {
+    }
+}
