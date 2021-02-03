935465679c3a8012a4e4dbe23f55e7dab2317a77
Waldo Navas <waldo9000@gmail.com>
2021-02-03 15:40:12 -0600

    Commit inicial de DTO de Datos de Seguimiento

---
+++ EnergymApp/EnergymApp.API.Aplicacion/DTOs/Clientes/DatosSeguimiento/DatosSeguimientoDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnergymApp.API.Aplicacion.DTOs.Clientes.DatosSeguimiento
+{
+    public class DatosSeguimientoDTO
+    {
+        public int IdCampoSeguimiento { get; set; }
+        public int IdCliente { get; set; }
+        public string DatoSeguimiento { get; set; }
+        public DateTime FechaRegistro { get; set; }
+    }
+}
