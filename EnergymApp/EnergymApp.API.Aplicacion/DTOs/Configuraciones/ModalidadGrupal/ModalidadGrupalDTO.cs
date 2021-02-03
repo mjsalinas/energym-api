b9f63029073c145591787e35f5faf58710afc62c
jorgemolina16@unitec.edu <jorgemolina16@unitec.edu>
2021-02-02 23:45:19 -0600

    Cambiar el Nombre de las Carpetas, Crear el repositorio demodalidad Grupal.

--- EnergymApp/EnergymApp.API.Aplicacion/DTOs/Configuraciones/ModalidadGrupal/ModalidadGrupalDTO.cs
+++ EnergymApp/EnergymApp.API.Aplicacion/DTOs/Configuraciones/ModalidadGrupal/ModalidadGrupalDTO.cs
@@ -2,14 +2,14 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace EnergymApp.API.Aplicacion.DTOs.Configuraciones.ModalidadGrual
+namespace EnergymApp.API.Aplicacion.DTOs.Configuraciones.ModalidadGrupal
 {
     public class ModalidadGrupalDTO
     {
         public int IdGrupo { get; set; }
         public int IdPlan { get; set; }
         public int LiderDeGrupo { get; set; }
-        public bool GrupoActivo { get; set; }
+        public sbyte GrupoActivo { get; set; }
         public string MensajeDeError { get; set; }
 
     }
---
+++ EnergymApp/EnergymApp.API.Infraestructura/Repositorios/Grupos/ModalidadGrupalRepo/ModalidadGrupalRepositorio.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EnergymApp.API.Domino.ModelosDB;
+using EnergymApp.API.Domino.Contexto;
+using System.Linq;
+using EnergymApp.API.Aplicacion.DTOs.Configuraciones.ModalidadGrupal;
+
+
+namespace EnergymApp.API.Infraestructura.Repositorios.Configuraciones.ModalidadGrupalRepo
+{
+        public class ModalidadGrupalRepositorio
+        {
+        private const string MensajeErrorInexistencia = "Unidad de Medida no existe";
+        public List<ModalidadGrupalDTO> ObtenerModalidadGrupal()
+        {
+            ContextoEnergym db = new ContextoEnergym();
+            List<ModalidadGrupal> modalidadesGrupales = db.ModalidadGrupal.ToList();
+            List<ModalidadGrupalDTO> modalidadGrupalDTO = new List<ModalidadGrupalDTO>();
+
+            foreach (var modalidadGrupal in modalidadesGrupales)
+            {
+                modalidadGrupalDTO.Add(new ModalidadGrupalDTO
+                {
+                    IdGrupo = modalidadGrupal.IdGrupo,
+                    IdPlan = modalidadGrupal.IdPlan,
+                    LiderDeGrupo = modalidadGrupal.LiderGrupo,
+                    GrupoActivo = modalidadGrupal.GrupoActivo,
+                });
+            }
+            return modalidadGrupalDTO;
+        }
+
+        public ModalidadGrupalDTO GuardarUnidadMedida(ModalidadGrupalDTO modalidadGrupal)
+        {
+            try
+            {
+                ContextoEnergym db = new ContextoEnergym();
+                ModalidadGrupal modalidadGrupalEntidad = new ModalidadGrupal
+                {
+                    IdGrupo = modalidadGrupal.IdGrupo,
+                    IdPlan = modalidadGrupal.IdPlan,
+                    LiderGrupo = modalidadGrupal.LiderDeGrupo,
+                    GrupoActivo = modalidadGrupal.GrupoActivo,
+                };
+                db.ModalidadGrupal.Add(modalidadGrupalEntidad);
+                db.SaveChanges();
+                modalidadGrupal.IdPlan = modalidadGrupalEntidad.IdPlan;
+                return modalidadGrupal;
+            }
+            catch (Exception ex)
+            {
+                return new ModalidadGrupalDTO
+                {
+                    MensajeDeError = ex.Message
+                };
+            }
+        }
+
+        public ModalidadGrupalDTO ModificarUnidadMedida(ModalidadGrupalDTO ModalidadGrupal)
+        {
+            try
+            {
+                ContextoEnergym db = new ContextoEnergym();
+
+                ModalidadGrupal modalidadGrupalEntidad = db.ModalidadGrupal.FirstOrDefault(camSeg => camSeg.IdGrupo == ModalidadGrupal.IdGrupo);
+                if (modalidadGrupalEntidad == null)
+                {
+                    return new ModalidadGrupalDTO
+                    {
+                        MensajeDeError = MensajeErrorInexistencia
+                    };
+                };
+                modalidadGrupalEntidad.IdGrupo = ModalidadGrupal.IdGrupo;
+                modalidadGrupalEntidad.IdPlan = ModalidadGrupal.IdPlan;
+                modalidadGrupalEntidad.LiderGrupo = ModalidadGrupal.LiderDeGrupo;
+                modalidadGrupalEntidad.GrupoActivo = ModalidadGrupal.GrupoActivo;
+                db.SaveChanges();
+                return ModalidadGrupal;
+            }
+            catch (Exception ex)
+            {
+                return new ModalidadGrupalDTO
+                {
+                    MensajeDeError = ex.Message
+                };
+            }
+        }
+
+    }
+        }
+    
