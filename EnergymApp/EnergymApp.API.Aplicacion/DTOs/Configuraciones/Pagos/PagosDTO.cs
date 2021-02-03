5031a1b0ef9030b8a659e876891f368cfee4655d
jorgemolina16@unitec.edu <jorgemolina16@unitec.edu>
2021-02-02 21:22:18 -0600

    Agrege los DTOS y Repositorios de Modalidad Grupal y Pagos.

---
+++ EnergymApp/EnergymApp.API.Aplicacion/DTOs/Configuraciones/Pagos/PagosDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnergymApp.API.Aplicacion.DTOs.Configuraciones.Pagos
+{
+    public class PagosDTO
+    {
+        public int IdPago { get; set; }
+        public int IdCliente { get; set; }
+        public string EstadoDePago { get; set; }
+        public DateTime FechaDeMaximaDePago{ get; set; }
+        public string MensajeDeError { get; set; }
+
+    }
+}
